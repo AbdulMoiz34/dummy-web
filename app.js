4582da37786c2079a839a8a840a4295dd2c33273
Abdul Moiz <moiz05366@gmail.com>
2023-08-25 14:30:20 +0500

    my first web
    when i learning javascript , so i make this website just for practice

---
+++ app.js
@@ -0,0 +1,563 @@
+
+
+// alert(num1)
+// alert( total+" " +"This is a Result"+ " " +"bro")
+
+// var abdul = 1000-120/2-40+100-1000-12+6;
+// alert('My Name Is Abdul Moiz I am 18 years old and i m from karachi')
+// alert("Thank you for submitting")
+
+// document.write("<center>"+ advanceAbdul+"</center>")
+
+// document.write("Abdul Moiz <br> I am 18 years old <br> im from karachi <br> i belong Muslim");
+
+// var abdulMoiz = 900+25-4-900-1000
+// console.log(abdulMoiz)
+
+// var a = 10;
+// var b = 15;
+// var c = 20;
+// var d = 25;
+// var total = a++ + ++a + a++ + ++a + --a + a-- - a-- + ++a + ++d - --d;
+// document.write("10 + 12 + 12 + 14 + 118 + 13 - 12 + 12 + 26 - 25")
+// document.write("<br>" + "= " + total)
+// alert(total)
+
+// var abdul = 1200 + 3 + 57 + 162728 + 17817811 - 2009885
+// alert(abdul)
+// document.write("<br>" + abdul)
+// console.log(abdul + " nikal beta")
+
+// var a = 5;
+// var b = 10;
+// var c = 15;
+// var d = 20;
+
+// var total = a + a++ + ++a - ++b + ++c + d++ - ++b + ++c - --d - b-- - ++b + ++c - ++a - --b - --c
+//    5 + 5   +  7   - 11  +  16 + 20  -  12  + 17 -  20  - 12 - 12  +  18 - 8  - 11  -  17
+
+// alert(total)
+// document.write(total)
+
+
+
+// prompt  (User se input lena )
+
+// prompt(2*10,"Abdul Moiz (Abdul Gaming)")
+// document.write(p)
+// alert("abdul")
+
+
+// var a = prompt("what is your Name ?", "Abdul Moiz")
+// var b = prompt("what is your age ?", "20")
+// var c = prompt("what is your contact number ?")
+
+
+// document.write(a + "<hr>" + b + "<hr>" + c)
+
+
+// Concepts of "PROMPT"
+// var fhint1 = "what is your name ?"
+// var fhint2 = "Abdul Moiz";
+// var fname = prompt(fhint1, fhint2);
+
+// var agehint1 = "20";
+// var agehint2 = "what is your age ?";
+// var age = prompt(agehint2, agehint1);
+
+// var usercontact = prompt("what is your contact number ?", "123456789")
+// document.write(fname + "<hr>" + age + " <hr>" + usercontact);
+
+// var mynum = 1200
+// var usernum =  +prompt()
+// document.write(mynum*usernum)
+// console.log(typeof a)
+
+
+// if()
+
+//                         " CONCEPTS OF MARKSHEET "
+
+// var userNum = +prompt("Enter Percentage")
+// var congratulation = "Mubarak ho bhai apka A+ agaya"
+// console.log(typeof userNum)
+
+// if (userNum >= 80 && userNum <= 99) {
+//     document.write(congratulation)
+// }
+
+
+// else if (userNum == 100) {
+//     document.write("You got 100% in All Subjects")
+// }
+
+// else if (userNum <= 79) {
+//     document.write("You Are Fail Bro/sis Betterluck Next Time")
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i+ " " + "january" + "<br>")
+// }
+
+
+
+// alert("Thank You For Submitting The Form")
+
+// var num1 = 5;
+// var num2 = 10;
+// var total = num1+num2;
+// // document.write(total)
+// var user = +prompt("Enter Number","23456789")
+// document.write(total+user)
+// console.log(typeof user)
+
+
+
+// var a = ["abdul moiz"]
+// document.write(a)
+
+// var num1 = 25
+// var userNum = +prompt("Enter Name ")
+// document.write(userNum+num1)
+
+// document.write("abdul <hr>Moiz <hr>")
+
+
+// "MARKSHEET"
+
+
+// var a = prompt("Enter Percentage")
+// if (a >= 80 && a < 100) {
+//     document.write("Your Grade is A<sup>+</sup>")
+// }
+
+// else if (a >= 60 && a < 80) {
+//     document.write("Your Grade is 'A'")
+// }
+
+// else {
+//     document.write("Better Luck Next Time 'Fail'")
+// }
+
+
+
+// var num1 = 10;
+// var num2 = 15;
+// var num3 = 25;
+// var total = num1++ + ++num3 + num2++ + num3++ + num1 + num2 + num1++ + ++num3 - --num1 + --num3 + ++num1 - --num2
+// 10     +  26    +  15    +   26  +  11   +  16  +   11   +   28    -  11    +  27   +   12   -  15
+// var a = 44
+// document.write(total * a)
+// var num = 10;
+
+
+
+// var num = prompt("Enter Percentage", "891")
+// var output = "Congratulation your grade is "
+
+// if (num >= 80 && num < 100) {
+//     document.write(output + "A+")
+// }
+
+// else if (num < 80 && num >= 70) {
+//     document.write(output + "A")
+// }
+
+// else if (num < 70 && num >= 50) {
+//     document.write(output + "B")
+// }
+
+// 5 + 3 sq -6 *4
+// 5  +    9    - 24
+
+
+// 14   - 24
+
+
+// var num1 = 15;
+// var num2 = 20;
+
+//  var total = num1 + num2 - num1 * num1 / num2 + (num2 * num1)
+//     15 + 20    - 15  * 15 / 20   + (20*15)
+//     15 + 20   -  15   *  0.75     + (300)
+//     15 + 20   -  11.25  + 300
+//     35   - 11.25
+//      23.75
+
+
+// document.write(total)
+
+
+// var x, y, z;
+// x = 25;
+// y = 20;
+// z = x + y;
+
+// var blank = "The value of z is " + z + ".";
+// alert(blank)
+
+
+// var x = 5;
+// var y = "50";
+
+// alert(x < y && y)
+
+// var num1 = "Hello"
+// var num2 = "World"
+// var num3 = "<br>"
+// var total = num1 + num3 + num2
+// document.write(total)
+
+
+// var num1 = 10;
+// var num2 = 15;
+// var total = num1++ + num2 + ++num2 - num1++ + num2 + num1++ + ++num1 * num2 -num1;
+//             // 10     + 15   +  16     - 11    +   16  +  12   +  14   *   16 - 14
+//             // 10     +  15   + 16    - 11     +    16  + 12   +   224 -14
+
+
+//             alert(total)
+
+
+// var a = 5;
+// var b = 10;
+// var c = --b +  ++a + a++ - b-- + b++ + ++b + a++;
+//         //   9  + 6  +  6  - 9   + 8   +  10  + 7
+
+// // document.write(c)
+
+// var hint1 = "Abdul Moiz";
+// var hint2 = "18";
+// var hint3 = "123456789";
+
+
+// var userName = prompt("what is your name?", hint1);
+// var userAge = prompt("what is your age?", hint2);
+// var userCell = prompt("what is your phone number?", hint3);
+
+
+// document.write("Your Name is " + userName + "." + "<br>");
+// document.write("your age is " + userAge + "." + "<br>");
+// document.write("your phone number is " + userCell + "." + "<br>");
+
+
+// var myNum = 10;
+// var userNum = prompt("Enter Number", "1");
+// console.log(typeof userNum)
+
+
+// document.write(myNum + " " + userNum);
+
+
+
+
+
+
+
+
+
+// var userNum = prompt("Enter Number");
+// var myNum = 10;
+// var userNum = parseInt(prompt("Enter Number"))
+// var userNum = +prompt("Enter Number")
+// document.write(userNum + myNum + "<br>");
+// document.write(typeof userNum)
+
+
+// var userName = prompt("Enter Name");
+// var sub1 = prompt("Computer");
+// var sub2 = prompt("Islamiat");
+// var sub3 = prompt("Physics");
+// var sub4 = prompt("Urdu");
+// var sub5 = prompt("Maths");
+
+
+// document.write("Your Name is " + userName + "." + "<br>");
+// document.write("In Computer =" + sub1 + "." + "<br>");
+// document.write("In Islamiat =" + sub2 + "." + "<br>");
+// document.write("In Physics =" + sub3 + "." + "<br>");
+// document.write("In Urdu =" + sub4 + "." + "<br>");
+// document.write("In Maths =" + sub5 + "." + "<br>");
+
+
+// ===========================================
+// var totNum = +prompt("Enter Total Marks Please.");
+// var obtNum = +prompt("Enter Obtained marks.");
+// var percentage = obtNum / totNum * 100;
+// var ans = "Your Percentage is ";
+
+// document.write(ans + percentage + "%.");
+
+
+
+//         ARRAY
+
+// .push(Last sy exciting array me add hota hy)
+// .pop(last sy kisi bhi value/element ko delete karta hy)
+// .unshift(Starting sy add karta hy)
+// .shift(starting sy delete karta hy)
+// splice accept three parameter
+// .splice (start kaha sy karna hy{index},delete kitny karne hy,replacement me dena hy?(optional))
+// .slice====> is used for copy arrays or value  .slice(index btao,length batao kitny copy karna hy )
+
+
+// var collection = ["abc","def","ghi","jkl","mno","pqr","stu"];
+// collection.push("x","y","z")
+// collection.pop()
+// collection.pop()
+// collection.unshift(1,2,3,4,"abdul")
+// // collection.shift()
+// // collection.shift()
+
+// var dosracoll = collection.slice(0,4)
+// console.log(dosracoll)
+// collection.splice(0,2)
+// collection.splice(7,4,"abdul")
+// collection.splice(3,4,"Moiz")
+// document.write(collection)
+// ======================================simple table make==============================
+
+// var user = prompt("Enter Number");
+// var userLength = prompt("Enter Length, kaha tk table chah hye by default 10 L.");
+// for (var i = 1; i <= userLength; i++) {
+//     document.write(user + " X " + i + " = " + i * user + "<br>")
+
+// }
+// if (userLength === "") {
+//     for (var i = 1; i <= 10; i++) {
+//         document.write(user + " X " + i + " = " + i * user + "<br>")
+//     }
+// }
+
+// ====================================================================
+
+
+
+//        toLowerCase =======>  sary small letters hojaingy;
+//        toUpperCase =======>   Sary alphabets capital hojaingy
+
+
+// Let's try;
+
+
+// var arr = ["Pakistan","India","Australia","Bangladesh","sri lanka"];
+
+// var myName = "abdUL mOIz";
+// var a = myName.slice(0, 1).toUpperCase();
+// var b = myName.slice(1, 5).toLowerCase();
+// var c = myName.slice(6, 7).toUpperCase();
+// var d = myName.slice(7).toLowerCase();
+// document.write(a + b + " " + c + d+".")
+
+// var myName = "abDul";
+// var copy = myName.slice(0, 1).toUpperCase();
+// var copy2 = myName.slice(1).toLowerCase();
+// document.write(copy + copy2)
+
+// ===============================================
+// var userInp = prompt("Enter Country");
+// var country = ["Karachi", "Peshawar", "Islamabad", "Sukkur", "Balochistan"];
+
+// for (var i = 0; i < country.length; i++) {
+// if(userInp==="Karachi") {
+// document.write(userInp+" hi jeety ga!")
+// break
+// }
+// }
+
+// ===============================================
+
+
+
+//   ============================== "SIMPLE CALCULATOR" =========================================
+
+
+// var userInp1 = +prompt("Enter Number");
+// var operator = prompt("Enter sign + , - , * , / , %. ");
+// var userInp2 = +prompt("Enter Number");
+// var add = userInp1 + userInp2;
+// var sub = userInp1 - userInp2;
+// var mul = userInp1 * userInp2;
+// var div = userInp1 / userInp2;
+// var modu = userInp1 % userInp2;
+
+// if (operator === "+") {
+//     document.write(userInp1 + " + " + userInp2 + " = " + add + ".")
+// } else if (operator === "-") {
+//     document.write(userInp1 + " - " + userInp2 + " = " + sub + ".")
+// } else if (operator === "*") {
+//     document.write(userInp1 + " * " + userInp2 + " = " + mul + ".")
+// } else if (operator === "/") {
+//     document.write(userInp1 + " / " + userInp2 + " = " + div + ".")
+// } else if (operator === "%") {
+//     document.write(userInp1 + " % " + userInp2 + " = " + modu + ".")
+// } else {
+//     document.write("<center>" + "Wrong Input" + "</center>")
+// }
+
+
+// =========================================
+
+
+// var userInp1 = +prompt("Enter Number.");
+// var operator = prompt("Enter sign");
+// var userInp2 = +prompt("Enter Number.");
+// var operation = [userInp1 + userInp2, userInp1 - userInp2, userInp1 * userInp2, userInp1 / userInp2]
+
+// if (operator === "+") {
+//     document.write(userInp1 + " + " + userInp2 + "." + "<br>" + "Your Answer" + " = " + operation[0] + ".")
+// }
+// else if (operator === "-") {
+
+//     document.write(userInp1 + " - " + userInp2 + "." + "<br>" + "Your answer = " + operation[1] + ".")
+// }
+// else if (operator === "*") {
+//     document.write(userInp1 + " * " + userInp2 + "." + "<br>" + "Your answer = " + operation[2] + ".")
+// }
+// else if (operator === "/") {
+//     document.write(userInp1 + " /" + userInp2 + "." + "<br>" + "Your answer = " + operation[3] + ".")
+
+// }
+// else {
+//     document.write("<center>" + "<h2>Wrong Input.</h2>" + "</center>")
+// }
+
+
+// =========================================
+// ASSIGNMENT 1 ka part 1
+
+// var a = +prompt("Enter any number.");
+// document.write("Result: <br>" + "The value of a is: " + a);
+// document.write("<br>-----------------------<br>");
+
+// //  Pre INcrement ++a
+// document.write("<br>The value of ++a is: " + ++a);
+// document.write("<br> Now The value of a is: " + a);
+
+// // Post Increment a++
+// document.write("<br><br>" + "The value of a++ is: " + a++);
+// document.write("<br>Now the value of a++ is: " + a);
+
+// // Pre Decrement --a
+// document.write("<br><br>" + " The Value of --a is: " + --a);
+// document.write("<br>Now the value of a is: " + a);
+
+
+// // Post Decremnet a--
+// document.write("<br><br>" + "The Value of a-- is: " + a--);
+// document.write("<br>" + "Now The Value of a is: " + a);
+
+//-------------------------------------------
+
+
+//  Ass1 part 2
+
+// var a = 2;
+// var b = 1;
+
+// var result = --a - --b + ++b + b--;
+//               1  -   0 +  1  + 1;
+//                    =  3;
+
+
+// var first = --a;
+// var sec = first - --b;
+// var third = sec + ++b;
+// var four = third + b--;
+
+// document.write("2) &nbsp; If a = 2 & b = 1, so find <br> <i>--a - --b + ++b + b-- = ?.</i> <hr>");
+
+// document.write("--a = " + first + ".<br>");
+// document.write("--a - --b = " + sec + ".<br>");
+// document.write("--a - --b + ++b = " + third + ".<br>");
+// document.write("--a - --b + ++b + b-- = " + four + "." + "<br> <br>");
+
+// document.write("<b>Result:</b>" + " " + four + ".")
+
+//-------------------------------------------
+
+
+// ASS 1 part 3 
+
+// var user = prompt("Enter Name.", "Ishaq Bhojani");
+// document.write("Assalam o alikum.<br>" + "'" + user + "'.");
+
+//-------------------------------------------
+
+
+// ASS 1 part 4
+
+
+//  2 X 1 = 2 
+
+// var userInp = +prompt("Enter Number", 5);
+// for (var i = 1; i <= 10; i++) {
+//     document.write(userInp + " X " + i + " = " + userInp * i + "<br>");
+// }
+
+//-------------------------------------------
+
+
+// ASS 1 part 5 
+
+
+// User 'Subject Name' Inputs
+// var sub1 = prompt("Enter 1st Subject name");
+// var sub2 = prompt("Enter 2nd Subject name");
+// var sub3 = prompt("Enter 3rd Subject name");
+
+// // user 'Subject Marks' Inputs
+// var sub1Marks = +prompt("Enter marks: '" + sub1 + "'");
+// var sub2Marks = +prompt("Enter marks: '" + sub2 + "'");
+// var sub3Marks = Number(prompt("Enter marks: '" + sub3 + "'"));
+// var userTotalMarks = sub1Marks + sub2Marks + sub3Marks;
+
+// //  calculations: Percentage
+// var sub1Per = sub1Marks / 100 * 100;
+// var sub2Per = sub2Marks / 100 * 100;
+// var sub3Per = sub3Marks / 100 * 100;
+
+// var percentage = (userTotalMarks / 300 * 100);
+
+// // Subject 1 
+// document.write("<b>" + sub1 + "</b>");
+// document.write("<br> Marks is " + sub1Marks + ".");
+// document.write("<br> Percentage is " + sub1Per + "%");
+
+// document.write("<br> ----------------------------<br>");
+
+// // sybject 2
+// document.write("<br><b>" + sub2 + "</b>");
+// document.write("<br> Marks is " + sub2Marks + ".");
+// document.write("<br> Percentage is " + sub2Per + "%");
+
+// document.write("<br> ----------------------------<br>");
+
+// // subject 3 
+// document.write("<br><b>" + sub3 + "</b>");
+// document.write("<br> Marks is " + sub3Marks + ".");
+// document.write("<br> Percentage is " + sub3Per + "%");
+
+// // Total Marks & obmarks...
+// document.write("<hr>" + "<b>Total Marks</b> = 300. <br> <b>Obtained Marks = </b>" + userTotalMarks);
+
+// // obtained percentage;
+
+// if (percentage >= 50) {
+//     alert("Congratulation Your Percentage = " + percentage + "%")
+// }
+
+
+var userInp = +prompt("Enter Number 1 to 10...");
+var myNum = Math.round(Math.random() * 10);
+
+if (userInp === myNum) {
+    alert("You win.");
+} else if (userInp + 1 === myNum || userInp - 1 === myNum) {
+    alert("You are so close.My num is: " + myNum);
+} else {
+    alert("You lost. My num is: " + myNum);
+}
+
+
+
